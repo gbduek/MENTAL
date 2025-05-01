13818b35e4bb1dada2c8fb51492f274aecbd8b44
Rafael Sagues <rafasagues@gmail.com>
2025-05-01 20:01:45 +0200

    forced movement

--- Assets/Scripts/Player/MovementProfile.cs
+++ Assets/Scripts/Player/MovementProfile.cs
@@ -1,10 +1,12 @@
+using System.Runtime.Serialization;
+using System.Security.Cryptography;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "MovementProfile", menuName = "Scriptable Objects/MovementProfile")]
 public class MovementProfile : ScriptableObject
 {
     public float speed = 10.0f;
-    //public float acceleration = 5f;
-    //public Vector2 forcedDirection = Vector2.zero;
-    //public float forcedStrength = 0f;
+
+    public bool isForcedMovement = false;
+    public float forcedSpeed = 5f;
 }
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,17 +1,34 @@
+using System.Security.Cryptography;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
     public MovementProfile profile;
+    public Transform forcedTarget;
 
     private Vector2 movementInput;
+
+    void Start()
+    {
+        forcedTarget = GameObject.FindWithTag("AttractedTo").transform;
+    }
 
     void Update()
     {
         // Apply movement every frame
         Vector3 movement = new Vector3(movementInput.x, movementInput.y, 0f);
         transform.position += movement * profile.speed * Time.deltaTime;
+        // If forced movement is enabled, move towards the target
+        if (profile.isForcedMovement && forcedTarget != null)
+        {
+            Vector3 targetPosition = forcedTarget.position;
+            Vector3 direction = (targetPosition - transform.position).normalized;
+            transform.position += direction * profile.forcedSpeed * Time.deltaTime;
+
+        }
+
+
     }
 
     // This function is automatically called by the new Input System
