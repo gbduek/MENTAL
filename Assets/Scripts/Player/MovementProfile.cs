65099871e71976603838a87e10d345f0c09967bd
Rafael Sagues <rafasagues@gmail.com>
2025-05-01 16:23:55 +0200

    new movement structure w/ movement profile

---
+++ Assets/Scripts/Player/MovementProfile.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "MovementProfile", menuName = "Scriptable Objects/MovementProfile")]
+public class MovementProfile : ScriptableObject
+{
+    public float speed = 10.0f;
+    //public float acceleration = 5f;
+    //public Vector2 forcedDirection = Vector2.zero;
+    //public float forcedStrength = 0f;
+}
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -3,14 +3,15 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 10.0f;
+    public MovementProfile profile;
+
     private Vector2 movementInput;
 
     void Update()
     {
         // Apply movement every frame
         Vector3 movement = new Vector3(movementInput.x, movementInput.y, 0f);
-        transform.position += movement * speed * Time.deltaTime;
+        transform.position += movement * profile.speed * Time.deltaTime;
     }
 
     // This function is automatically called by the new Input System
