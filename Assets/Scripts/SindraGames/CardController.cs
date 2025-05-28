e17b9d940b674629e803c2e4c05a483844c6496e
gbduek <94713966+gbduek@users.noreply.github.com>
2025-05-28 12:31:32 -0300

    CardMiniGame 1.0

--- Assets/Scripts/SindraGames/CardController.cs
+++ Assets/Scripts/SindraGames/CardController.cs
@@ -1,30 +1,117 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
-public class CardController : MonoBehaviour
+public static class RectTransformExtensions
 {
+    public static bool IsOverlapping(RectTransform rect1, RectTransform rect2)
+    {
+        Vector3[] corners1 = new Vector3[4];
+        Vector3[] corners2 = new Vector3[4];
+
+        rect1.GetWorldCorners(corners1);
+        rect2.GetWorldCorners(corners2);
+
+        Rect r1 = new Rect(corners1[0], corners1[2] - corners1[0]);
+        Rect r2 = new Rect(corners2[0], corners2[2] - corners2[0]);
+
+        return r1.Overlaps(r2);
+    }
+}
+
+public class CardController
+    : MonoBehaviour,
+        IPointerDownHandler,
+        IBeginDragHandler,
+        IDragHandler,
+        IEndDragHandler
+{
+    private RectTransform rectTransform;
+    private Vector2 originalPosition;
+    public Image image;
+
+    [HideInInspector]
+    public string cardID;
+    private bool isMovable = false;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private void Awake()
     {
-        
+        rectTransform = GetComponent<RectTransform>();
+        image = GetComponent<Image>();
     }
 
-    void OnMouseDown()
+    public void SetupCard(string id)
     {
-        // Handle card click event
-        Debug.Log("Card clicked: " + gameObject.name);
-        // Add your card flip logic here
+        cardID = id;
+
+        // Example conditional logic:
+        if (id == "Card1")
+        {
+            image.sprite = Resources.Load<Sprite>("Sprites/MemoryGame/king_of_hearts2");
+        }
+        else if (id == "Card2")
+        {
+            image.sprite = Resources.Load<Sprite>("Sprites/MemoryGame/queen_of_diamonds2");
+            transform.position += new Vector3(-600, 0, 0); // Set the position of Card2
+        }
+        else if (id == "Card3")
+        {
+            image.sprite = Resources.Load<Sprite>("Sprites/MemoryGame/7_of_clubs");
+            transform.position += new Vector3(-600, -300, 0); // Set the position of Card3
+        }
+        else
+        {
+            Debug.LogWarning("Unknown Card ID: " + id);
+        }
     }
 
-    void OnMouseUp()
+    public void OnPointerDown(PointerEventData eventData)
     {
-        // Handle card release event
-        Debug.Log("Card released: " + gameObject.name);
-        // Add your card flip logic here
+        originalPosition = rectTransform.anchoredPosition;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnDrag(PointerEventData eventData)
     {
-        
+        rectTransform.anchoredPosition += eventData.delta;
+    }
+
+    private bool IsMatch(CardController other)
+    {
+        return (this.cardID == "Card1" && other.cardID == "Card2")
+            || (this.cardID == "Card2" && other.cardID == "Card1");
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        foreach (CardController otherCard in FindObjectsOfType<CardController>())
+        {
+            if (otherCard != this && IsOverlapping(otherCard))
+            {
+                Debug.Log("Overlapping with: " + otherCard.cardID);
+
+                if (IsMatch(otherCard))
+                {
+                    Debug.Log("Match found between " + cardID + " and " + otherCard.cardID);
+                    // Snap cards, play effect, etc.
+                    return;
+                }
+            }
+        }
+
+        // If no match, return to original position
+        rectTransform.anchoredPosition = originalPosition;
+        isMovable = true; // Allow dragging again
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        Debug.Log("Begin Drag");
+        isMovable = false;
+    }
+
+    public bool IsOverlapping(CardController other)
+    {
+        return RectTransformExtensions.IsOverlapping(rectTransform, other.rectTransform);
     }
 }
--- Assets/Scripts/SindraGames/MemoryGame.cs
+++ Assets/Scripts/SindraGames/MemoryGame.cs
@@ -2,29 +2,40 @@ using UnityEngine;
 
 public class MemoryGame : MonoBehaviour
 {
-    public Transform canvasChild;
+    public GameObject canvasChild;
     public GameObject[] cards; // Array to hold the card GameObjects
     public GameObject cardPrefab; // Prefab for the card GameObject
     public int numberOfCards = 1; // Number of cards to create
+    private bool gameStarted = false;
 
-    void OnTriggerEnter(Collider other)
+    void OnTriggerEnter2D(Collider2D other)
     {
         Debug.Log("OnTriggerEnter called with: " + other.name);
-        if(other.CompareTag("Player"))
+        if (!gameStarted)
         {
-            Debug.Log("Player entered the trigger area");
-            // Start the game when the player enters the trigger
-            InitializeGame();
+            if (other.CompareTag("Player"))
+            {
+                Debug.Log("Player entered the trigger area");
+                // Start the game when the player enters the trigger
+                InitializeGame();
+                gameStarted = true;
+            }
         }
     }
 
     // Initialize the game by creating card instances
     void InitializeGame()
     {
-        canvasChild.gameObject.GetComponent<Canvas>().enabled = true;
-        cards = new GameObject[1];
-        cards[0] = Instantiate(cardPrefab, canvasChild);
+        Time.timeScale = 0;
+        canvasChild.SetActive(true);
+        cards = new GameObject[3];
 
-        cards[0].SetActive(true);
+        for (int i = 0; i < cards.Length; i++)
+        {
+            cards[i] = Instantiate(cardPrefab, canvasChild.transform);
+            CardController cardController = cards[i].GetComponent<CardController>();
+            cardController.SetupCard("Card" + (i + 1).ToString());
+            cards[i].SetActive(true);
+        }
     }
 }
