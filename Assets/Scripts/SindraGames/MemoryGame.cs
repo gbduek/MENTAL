1e9a3d48b0a528da981b824d84b00e281c18acf1
gbduek <94713966+gbduek@users.noreply.github.com>
2025-05-28 09:51:11 -0300

    .

--- Assets/Scripts/SindraGames/MemoryGame.cs
+++ Assets/Scripts/SindraGames/MemoryGame.cs
@@ -2,27 +2,28 @@ using UnityEngine;
 
 public class MemoryGame : MonoBehaviour
 {
+    public Transform canvasChild;
     public GameObject[] cards; // Array to hold the card GameObjects
     public GameObject cardPrefab; // Prefab for the card GameObject
     public int numberOfCards = 1; // Number of cards to create
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+
+    void OnTriggerEnter(Collider other)
     {
-
-        InitializeGame();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
+        Debug.Log("OnTriggerEnter called with: " + other.name);
+        if(other.CompareTag("Player"))
+        {
+            Debug.Log("Player entered the trigger area");
+            // Start the game when the player enters the trigger
+            InitializeGame();
+        }
     }
 
     // Initialize the game by creating card instances
     void InitializeGame()
     {
+        canvasChild.gameObject.GetComponent<Canvas>().enabled = true;
         cards = new GameObject[1];
-        cards[0] = Instantiate(cardPrefab, this.transform);
+        cards[0] = Instantiate(cardPrefab, canvasChild);
 
         cards[0].SetActive(true);
     }
